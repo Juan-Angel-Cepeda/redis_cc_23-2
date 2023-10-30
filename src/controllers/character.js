ceca96196f6f755560254d4289afd87e797dba31
angelcepeda <jangelcepeda@protonmail.com>
2023-10-30 13:45:00 -0600

    mostrando la respuesta de redis

--- src/controllers/character.js
+++ src/controllers/character.js
@@ -9,11 +9,17 @@ async function list(req,res,next){
     
     try{
         await client.connect();
+        const cachedCharacters = await client.get('characters');
+        
+        if(cachedCharacters){{
+            console.log('Datos obtenidos de redis');
+            console.log(cachedCharacters);
+        }
         const response = await axios.get('https://rickandmortyapi.com/api/character');
         await client.set('characters',JSON.stringify(response.data));
         console.log('Datos guardados en redis');
         res.json(response.data);
-    }catch(err){
+    }}catch(err){
         console.log(err);
         res.status(500).send('Internal server error');
     }finally{
