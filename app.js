867919511e1a60bec56dca2cf8469d1d4297bf99
angelcepeda <jangelcepeda@protonmail.com>
2023-10-31 15:50:03 -0600

    port fixed i hope

--- app.js
+++ app.js
@@ -4,7 +4,4 @@ const charactersRouter = require('./routes/characters');
 const app = express();
 
 app.use('/characters',charactersRouter);
-
-app.listen(3000);
-console.log('Server on port',3000);
 module.exports = app;
